621a131a133eb7b5652e996737ac6bc1e8aff9fe
Hiromichi NOMATA <git@hiromichinomata.com>
2025-06-29 16:25:15 +0900

    add: templates

--- self_order_system/self_order_system/settings.py
+++ self_order_system/self_order_system/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'self_order',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,9 @@ ROOT_URLCONF = 'self_order_system.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / 'templates',
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
